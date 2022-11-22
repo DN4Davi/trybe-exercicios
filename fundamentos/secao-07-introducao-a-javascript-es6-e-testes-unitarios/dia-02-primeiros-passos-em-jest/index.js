f5ef45fe433f85423276edf575dbd123748e3063
Davi N. de Azevedo <dn4davi@pop-os.localdomain>
2022-11-22 13:34:56 -0300

    exercise 02

--- fundamentos/secao-07-introducao-a-javascript-es6-e-testes-unitarios/dia-02-primeiros-passos-em-jest/index.js
+++ fundamentos/secao-07-introducao-a-javascript-es6-e-testes-unitarios/dia-02-primeiros-passos-em-jest/index.js
@@ -1,3 +1,5 @@
-const myRemove = require("./myRemove");
+const myRemove = require("./exercicio-01/myRemove");
+const myFizzBuzz = require("./exercicio-02/myFizzBuzz")
 
-console.log(myRemove([1, 2, 3, 4], 1))
+console.log(myRemove([1, 2, 3, 4], 1));
+console.log(myFizzBuzz(15));
